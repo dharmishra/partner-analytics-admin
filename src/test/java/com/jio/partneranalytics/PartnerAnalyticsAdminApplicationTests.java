c5cf347fda2eb894a92efde54cedf703d2f346a6
Dharmendra Mishra <Dharmendra2.Mishra@ril.com>
2020-04-28 17:23:06 +0530

    Initial commit

---
+++ src/test/java/com/jio/partneranalytics/PartnerAnalyticsAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jio.partneranalytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartnerAnalyticsAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
